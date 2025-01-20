aafafe1e59e78148c2ec55a839df966fd36beb44
coolat1010 <ryohei632@gmail.com>
2025-01-20 17:28:09 +0900

    RouterLink追加

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
-import { RouterView } from 'vue-router'
+import { RouterView, RouterLink } from 'vue-router'
 </script>
 
 <template>
+  <ul>
+    <li>
+      <RouterLink :to="{ name: 'dashboard' }">Dashboard</RouterLink>
+    </li>
+    <li>
+      <RouterLink :to="{ name: 'login' }">Login</RouterLink>
+    </li>
+  </ul>
   <RouterView />
 </template>
-
-<style scoped></style>
