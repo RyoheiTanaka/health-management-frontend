b0e9253f619812454ff0e0a2c84fe0209150314c
coolat1010 <ryohei632@gmail.com>
2025-01-21 13:56:10 +0900

    axiosインストール

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 import './styles/tailwind.css'
+
+// Axiosのデフォルト設定
+axios.defaults.withCredentials = true
+axios.defaults.withXSRFToken = true
 
 const app = createApp(App)
 
