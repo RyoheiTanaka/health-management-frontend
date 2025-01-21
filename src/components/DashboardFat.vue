29793dcdbacc60961cc6c6596d2a3713245f1279
coolat1010 <ryohei632@gmail.com>
2025-01-21 14:05:05 +0900

    ログインページ、だっしゅダッシュボードページ作成

---
+++ src/components/DashboardFat.vue
@@ -0,0 +1,75 @@
+<script></script>
+
+<template>
+  <section class="px-6 py-12 lg:px-8">
+    <div class="bg-white p-10 rounded shadow border border-gray-200">
+      <h2 class="mb-8 text-xl font-bold text-black">体脂肪</h2>
+      <div class="flex flex-col">
+        <div class="grid grid-cols-2 rounded-sm bg-gray-200">
+          <div class="p-2.5 xl:p-5">
+            <h3 class="text-sm font-medium text-gray-500 xsm:text-base">日付</h3>
+          </div>
+          <div class="p-2.5 xl:p-5">
+            <h3 class="text-sm font-medium text-gray-500 xsm:text-base">体脂肪率</h3>
+          </div>
+        </div>
+        <div class="grid grid-cols-2 border-b border-stroke">
+          <div class="p-2.5 xl:p-5">
+            <p class="text-sm">1/1</p>
+          </div>
+          <div class="p-2.5 xl:p-5">
+            <p class="text-sm">21.6%</p>
+          </div>
+        </div>
+        <div class="grid grid-cols-2 border-b border-stroke">
+          <div class="p-2.5 xl:p-5">
+            <p class="text-sm">1/2</p>
+          </div>
+          <div class="p-2.5 xl:p-5">
+            <p class="text-sm">21.6%</p>
+          </div>
+        </div>
+        <div class="grid grid-cols-2 border-b border-stroke">
+          <div class="p-2.5 xl:p-5">
+            <p class="text-sm">1/3</p>
+          </div>
+          <div class="p-2.5 xl:p-5">
+            <p class="text-sm">21.6%</p>
+          </div>
+        </div>
+        <div class="grid grid-cols-2 border-b border-stroke">
+          <div class="p-2.5 xl:p-5">
+            <p class="text-sm">1/4</p>
+          </div>
+          <div class="p-2.5 xl:p-5">
+            <p class="text-sm">21.6%</p>
+          </div>
+        </div>
+        <div class="grid grid-cols-2 border-b border-stroke">
+          <div class="p-2.5 xl:p-5">
+            <p class="text-sm">1/5</p>
+          </div>
+          <div class="p-2.5 xl:p-5">
+            <p class="text-sm">21.6%</p>
+          </div>
+        </div>
+        <div class="grid grid-cols-2 border-b border-stroke">
+          <div class="p-2.5 xl:p-5">
+            <p class="text-sm">1/6</p>
+          </div>
+          <div class="p-2.5 xl:p-5">
+            <p class="text-sm">21.6%</p>
+          </div>
+        </div>
+        <div class="grid grid-cols-2">
+          <div class="p-2.5 xl:p-5">
+            <p class="text-sm">1/7</p>
+          </div>
+          <div class="p-2.5 xl:p-5">
+            <p class="text-sm">21.6%</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
