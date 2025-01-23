f0882c0af38a35e704e49b1aadcb931c610dd846
coolat1010 <ryohei632@gmail.com>
2025-01-23 13:49:34 +0900

    Badgeデータを取得し表示

--- src/components/DashboardBdge.vue
+++ src/components/DashboardBdge.vue
@@ -1,4 +1,21 @@
-<script></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const badges = ref({})
+const fetchData = async () => {
+  try {
+    const res = await axios.get('/api/fitbit/badges', { params: { is_dashboard: true } })
+    badges.value = res.data.data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+onMounted(async () => {
+  await fetchData()
+})
+</script>
 
 <template>
   <section class="px-6 py-12 lg:px-8">
@@ -6,50 +23,11 @@
       <h2 class="mb-8 text-xl font-bold text-black">獲得バッチ</h2>
       <div class="m-0 mx-auto">
         <ul class="flex justify-center">
-          <li class="mx-2 xl:mx-8">
+          <li class="mx-2 xl:mx-8" v-for="badge in badges" :key="badge.name">
             <div class="mb-5">
-              <img
-                src="https://www.gstatic.com/fitbit/badge/images/badges_new/125px/badge_goal_weight21k.png"
-                alt=""
-              />
+              <img :src="badge.image125px" :alt="badge.description" />
             </div>
-            <p class="text-center">バッチ名1</p>
-          </li>
-          <li class="mx-2 xl:mx-8">
-            <div class="mb-5">
-              <img
-                src="https://www.gstatic.com/fitbit/badge/images/badges_new/125px/badge_goal_weight21k.png"
-                alt=""
-              />
-            </div>
-            <p class="text-center">バッチ名2</p>
-          </li>
-          <li class="mx-2 xl:mx-8">
-            <div class="mb-5">
-              <img
-                src="https://www.gstatic.com/fitbit/badge/images/badges_new/125px/badge_goal_weight21k.png"
-                alt=""
-              />
-            </div>
-            <p class="text-center">バッチ名3</p>
-          </li>
-          <li class="mx-2 xl:mx-8">
-            <div class="mb-5">
-              <img
-                src="https://www.gstatic.com/fitbit/badge/images/badges_new/125px/badge_goal_weight21k.png"
-                alt=""
-              />
-            </div>
-            <p class="text-center">バッチ名4</p>
-          </li>
-          <li class="mx-2 xl:mx-8">
-            <div class="mb-5">
-              <img
-                src="https://www.gstatic.com/fitbit/badge/images/badges_new/125px/badge_goal_weight21k.png"
-                alt=""
-              />
-            </div>
-            <p class="text-center">バッチ名5</p>
+            <p class="text-center">{{ badge.short_name }}</p>
           </li>
         </ul>
       </div>
