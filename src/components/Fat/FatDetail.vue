e2a282d585da9e5dd045db286e85c2e5f3ded0e5
coolat1010 <ryohei632@gmail.com>
2025-01-29 09:25:11 +0900

    体脂肪率詳細作成

--- src/components/Fat/FatDetail.vue
+++ src/components/Fat/FatDetail.vue
@@ -1,46 +1,135 @@
 <script setup>
 import { ref, onBeforeMount } from 'vue'
-import { RouterLink } from 'vue-router'
-import { getBadgeListApi } from '@/apis/FitbitLog'
-import { groupBy } from '@/utils/format'
-import { Carousel, Slide, Navigation } from 'vue3-carousel'
-import 'vue3-carousel/carousel.css'
+import { useRoute } from 'vue-router'
+import { getFatApi } from '@/apis/FitbitLog'
 
-const carouselConfig = {
-  itemsToScroll: 4,
-  itemsToShow: 4,
+const route = useRoute()
+const isFetched = ref(false)
+const id = route.params.id
+const fat = ref({})
+const obesityLevel = ref('')
+
+const chartOptions = ref({
+  chart: {
+    id: 'vuechart-fat-detail',
+    type: 'radialBar',
+    height: 400,
+    toolbar: {
+      show: true,
+    },
+  },
+  plotOptions: {
+    radialBar: {
+      startAngle: -135,
+      endAngle: 135,
+      dataLabels: {
+        name: {
+          fontSize: '16px',
+          color: undefined,
+          offsetY: 120,
+        },
+        value: {
+          formatter: function (val) {
+            const percentage = (30 * val) / 100
+            const arr = String(percentage).split('.')
+            if (arr[1]) {
+              return percentage.toFixed(1) + '%'
+            } else {
+              return percentage.toFixed() + '%'
+            }
+          },
+          offsetY: 76,
+          fontSize: '22px',
+          color: undefined,
+        },
+      },
+    },
+  },
+  fill: {
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      shadeIntensity: 0.15,
+      inverseColors: false,
+      opacityFrom: 1,
+      opacityTo: 1,
+      stops: [0, 50, 65, 91],
+    },
+  },
+  stroke: {
+    dashArray: 4,
+  },
+  labels: ['Body Fat Percentage'],
+})
+
+const series = ref([])
+
+function getObesityLevel(fat) {
+  if (fat < 10) {
+    return 'やせ'
+  } else if (fat < 20) {
+    return '標準'
+  } else if (fat < 25) {
+    return '軽肥満'
+  } else {
+    return '肥満'
+  }
 }
 
-const badges = ref({})
-const categorybadges = ref({})
-
 onBeforeMount(async () => {
-  badges.value = await getBadgeListApi()
-  categorybadges.value = groupBy(badges.value, 'category')
+  isFetched.value = false
+  if (Object.keys(fat.value).length == 0 || fat.value.id != id) {
+    fat.value = await getFatApi(id)
+    series.value.push((fat.value.fat / 30) * 100)
+    obesityLevel.value = getObesityLevel(fat.value.fat)
+  }
+  isFetched.value = true
 })
 </script>
 
 <template>
   <section class="px-6 py-12 lg:px-8">
     <div class="bg-white p-10 rounded shadow border border-gray-200 min-h-60">
-      <h2 class="mb-8 text-xl font-bold text-black">獲得バッチ</h2>
-      <div class="py-12 m-0 mx-auto" v-for="(categorybadge, index) in categorybadges" :key="index">
-        <h3 class="pb-5 text-lg font-bold">{{ index }}</h3>
-        <Carousel v-bind="carouselConfig">
-          <Slide v-for="badge in categorybadge" :key="badge.id">
-            <div class="carousel__item min-h-40">
-              <RouterLink :to="{ name: 'badgeDetail', params: { id: badge.id } }">
-                <div class="mb-5">
-                  <img :src="badge.image125px" />
-                </div>
-                <p class="text-center">{{ badge.short_name }}</p>
-              </RouterLink>
+      <h2 class="mb-8 text-xl font-bold text-black">体脂肪率</h2>
+      <div v-if="isFetched" class="flex flex-col justify-normal lg:flex-row lg:justify-between">
+        <div id="card" class="m-0 mx-auto pb-8 lg:pb-0">
+          <div id="chart">
+            <apexchart
+              type="radialBar"
+              height="400"
+              :options="chartOptions"
+              :series="series"
+            ></apexchart>
+          </div>
+        </div>
+        <div class="lg:w-[50%]">
+          <div class="flex flex-col">
+            <div class="grid grid-cols-2 rounded-sm border-b border-stroke">
+              <div class="p-2.5 xl:p-5">
+                <h3 class="text-sm font-medium text-gray-500 xsm:text-base">計測日</h3>
+              </div>
+              <div class="p-2.5 xl:p-5">
+                <p class="text-sm font-medium text-black xsm:text-base">{{ fat.date }}</p>
+              </div>
             </div>
-          </Slide>
-          <template #addons="{ slidesCount }">
-            <Navigation v-if="slidesCount > 1" />
-          </template>
-        </Carousel>
+            <div class="grid grid-cols-2 rounded-sm border-b border-stroke">
+              <div class="p-2.5 xl:p-5">
+                <h3 class="text-sm font-medium text-gray-500 xsm:text-base">体脂肪率</h3>
+              </div>
+              <div class="p-2.5 xl:p-5">
+                <p class="text-sm font-medium text-black xsm:text-base">{{ fat.fat }}%</p>
+              </div>
+            </div>
+            <div class="grid grid-cols-2 rounded-sm border-b border-stroke">
+              <div class="p-2.5 xl:p-5">
+                <h3 class="text-sm font-medium text-gray-500 xsm:text-base">肥満度</h3>
+              </div>
+              <div class="p-2.5 xl:p-5">
+                <p class="text-sm font-medium text-black xsm:text-base">{{ obesityLevel }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
--- src/views/Fat/FatDetailView.vue
+++ src/views/Fat/FatDetailView.vue
@@ -1,11 +1,11 @@
 <script setup>
 import MainHeader from '@/components/Common/MainHeader.vue'
 import MainFooter from '@/components/Common/MainFooter.vue'
+import FatDetail from '@/components/Fat/FatDetail.vue'
 </script>
 
 <template>
   <MainHeader />
-  <h2>FatDetail</h2>
-  <p><RouterLink :to="{ name: 'fatList' }">一覧ページへ</RouterLink></p>
+  <FatDetail />
   <MainFooter />
 </template>
